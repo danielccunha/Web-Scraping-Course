0d22185a41b01d82983bb6eba79e40c3b9879d17
danielccunha <danielcunha54@gmail.com>
2019-02-06 22:35:16 -0200

    Implemented bot for logging repositories from github

---
+++ Bots/Bots.Github.Repositories/Models/Github.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+
+namespace Bots.Github.Repositories.Models
+{
+    public static class Github
+    {
+        public static void LogRepositories(string username)
+        {
+            const string XPath = "//*[@id=\"user-repositories-list\"]/ul/li";
+            HtmlDocument doc = GetHtmlDocument(username);
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(XPath);
+
+            Console.WriteLine($"\nRepositories ({nodes.Count}):");
+
+            foreach (HtmlNode node in nodes)
+            {
+                string name = node.InnerText.Split("\n")[5].Trim();
+                Console.WriteLine($"\t{name}");
+            }
+        }
+
+        private static HtmlDocument GetHtmlDocument(string username)
+        {
+            string url = $"https://github.com/{username}?tab=repositories";
+            string html = string.Empty;
+
+            using (WebClient client = new WebClient())
+                html = client.DownloadString(url);
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            return doc;
+        }
+    }
+}
---
+++ Bots/Bots.Github.Repositories/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bots.Github.Repositories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's the username? ");
+            string username = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(username))
+                Console.WriteLine("Invalid username.");
+            else
+                Models.Github.LogRepositories(username);
+
+            Console.ReadKey(intercept: true);
+        }
+    }
+}
